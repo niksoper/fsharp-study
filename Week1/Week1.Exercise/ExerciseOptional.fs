3b13f2c04e254998d261a4892569b2c40912d20e
Nick Soper <nsoper@scottlogic.com>
2015-04-13 23:24:21 +0100

    Completed optional exercise

--- Week1/Week1.Exercise/ExerciseOptional.fs
+++ Week1/Week1.Exercise/ExerciseOptional.fs
@@ -7,6 +7,7 @@
         |CoUk
         |Com
 
+    [<StructuredFormatDisplay("{FirstName}")>]
     type Contact = {
         FirstName : string
         LastName : string
@@ -18,16 +19,18 @@
         LongestNameContact : Contact
         LongestEmailContact : Contact
         CountContactsWithCoUk : int
-        CountContactWithCom : int
+        CountContactsWithCom : int
         TotalNumberOfContacts : int
     }
 
-    let InitStatsWithContact contact = {LongestNameContact = contact; LongestEmailContact = contact; CountContactsWithCoUk = 0; CountContactWithCom = 0; TotalNumberOfContacts = 0}
+    let InitStatsWithContact contact = {LongestNameContact = contact; LongestEmailContact = contact; CountContactsWithCoUk = 0; CountContactsWithCom = 0; TotalNumberOfContacts = 0}
 
     let FSharpStudyGroup = "Andrew Lee <alee@scottlogic.co.uk>; Chris Smith <csmith@scottlogic.com>; Ian Lovell <ilovell@scottlogic.co.uk>; Jason Ebbin <JEbbin@scottlogic.com>; John Dawes <JDawes@scottlogic.com>; Matthew Dunsdon <MDunsdon@scottlogic.co.uk>; Nicholas Soper <NSoper@scottlogic.com>; Richard Doyle <rdoyle@scottlogic.co.uk>; Sean O’Neill <SONeill@scottlogic.com>; Stephen Ford <SFord@scottlogic.co.uk>; Thomas Kelly <tkelly@scottlogic.com>; Tyler Ferguson <TFerguson@scottlogic.com>"
 
     //TODO Take a string and split it by ';' return a list of the split values
-    let splitStringBySemiColons (semiColonSepString : string) = ["contact1"; "contact2"; "contact3"]
+    let splitStringBySemiColons (semiColonSepString : string) = 
+        semiColonSepString.Split ';'
+        |> Array.toList
 
     //Take a contact string, E.g. Andrew Lee <alee@scottlogic.co.uk> and parse to the Contact Type
     //let parseContact (contact:string) = {FirstName = ""; LastName=""; Email = ""; EmailExtension = CoUk}
@@ -47,7 +50,10 @@
         {FirstName = firstName; LastName=lastName; Email = email; EmailExtension = emailExtension}
 
     //TODO Return "They have a UK domain Extension, YAY!" if the extension is .co.uk. Return "Why do they have a COM domain Extension? meh." if the extension is .com
-    let getExtensionPhrase (extension:DomainExtension)  = ""
+    let getExtensionPhrase (extension:DomainExtension)  = 
+        match extension with
+        | CoUk -> "They have a UK domain Extension, YAY!"
+        | Com -> "Why do they have a COM domain Extension? meh."
 
     (*
         TODO Return one of the following strings for each Contact
@@ -55,10 +61,12 @@
         or 
         "{FirstName} {LastName} has an email of {Email}. Why do they have a COM domain Extension? meh."
     *)
-    let getContactString (contact:Contact) = ""
+    let getContactString (contact:Contact) = 
+        let phrase = getExtensionPhrase contact.EmailExtension
+        sprintf "%s %s has an email of %s. %s." contact.FirstName contact.LastName contact.Email phrase
 
     //TODO create a function that calls ParseContact then calls GetContactString with the result of ParseContact
-    let transformContactString : (string -> string) = (fun a -> a.Trim())
+    let transformContactString : (string -> string) = (fun raw -> raw |> parseContact |> getContactString)
     
     (*
         
@@ -74,12 +82,26 @@
         The you should be able to do this function only with the functions created above, 
         
     *)
-    let prettyPrintFSharpGroupRecords (recordsList : string list) = ()
+    let prettyPrintFSharpGroupRecords (recordsList : string list) = 
+        recordsList
+        |> mapList parseContact
+        |> mapList getContactString
+        |> printList
 
+
+    let nameLength (contact: Contact) = contact.FirstName.Length + contact.LastName.Length
+    let emailLength (contact: Contact) = contact.Email.Length
 
     //TODO write an accumulator function which will take the current stats, a new contact, and return the updated statistics. 
     //This will be used in PrintRecordsStats
-    let StatsFolder (acc: ContactStats) (elem: Contact) : ContactStats= acc
+    let StatsFolder (acc: ContactStats) (elem: Contact) : ContactStats = 
+        let longName = if nameLength elem > nameLength acc.LongestNameContact then { acc with LongestNameContact = elem } else acc
+        let longEmail = if emailLength elem > emailLength longName.LongestEmailContact then { longName with LongestEmailContact = elem } else longName
+        let counted = 
+            match (acc, elem) with
+            | ({ CountContactsWithCoUk = count; TotalNumberOfContacts = total }, { EmailExtension = CoUk }) -> { longEmail with CountContactsWithCoUk = count + 1;  TotalNumberOfContacts = total + 1 }
+            | ({ CountContactsWithCom = count; TotalNumberOfContacts = total }, { EmailExtension = Com }) -> { longEmail with CountContactsWithCom = count + 1; TotalNumberOfContacts = total + 1 }
+        counted
 
     (*
         TODO The following should print the following lines, based on the records list: 
@@ -94,7 +116,16 @@
         Modify the ContactStats type above to help pretty print it. See : http://stackoverflow.com/a/13537020 and https://msdn.microsoft.com/en-us/library/ee370560.aspx
 
     *)
-    let printRecordsStats (recordsList : string list) = ()
+    let printRecordsStats (recordsList : string list) =
+        let parsed = recordsList |> mapList parseContact 
+        let initialStats = InitStatsWithContact parsed.Head
+        let stats = foldList StatsFolder initialStats parsed
+
+        printfn "%A has the longest full name" stats.LongestNameContact
+        printfn "%A has the longest email" stats.LongestEmailContact
+        printfn "There are %A contacts with an email with extension .co.uk" stats.CountContactsWithCoUk
+        printfn "There are %A contacts with an email with extension .com" stats.CountContactsWithCom
+        printfn "There are %d contacts in total" stats.TotalNumberOfContacts
 
     let executeExercise() = 
         let recordsList = splitStringBySemiColons FSharpStudyGroup
